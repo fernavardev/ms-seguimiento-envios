b53a1886187c0435d8d19e432a83f4ec7192c222
Fernando <fer.navarrog@duocuc.cl>
2026-03-27 03:24:57 -0300

    inicializacion del proyecto y configuracion estructura base

---
+++ src/main/java/com/fernavardev/ms_seguimiento_envios/MsSeguimientoEnviosApplication.java
@@ -0,0 +1,13 @@
+package com.fernavardev.ms_seguimiento_envios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSeguimientoEnviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSeguimientoEnviosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernavardev/ms_seguimiento_envios/MsSeguimientoEnviosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernavardev.ms_seguimiento_envios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSeguimientoEnviosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
